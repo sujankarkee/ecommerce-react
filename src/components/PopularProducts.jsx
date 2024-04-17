ffdd6596eb4c70cbdeb7bd070084483cab51a097
sujankarkee <sujan451@gmail.com>
2024-04-17 16:52:12 +0545

    added hey

--- src/components/PopularProducts.jsx
+++ src/components/PopularProducts.jsx
@@ -16,6 +16,7 @@ function PopularProducts() {
       <div className="w-64 h-80 border-2 border-gray-200">
       <img className="w-48 min-h-60" src={images.greenApple} alt="#" />
       <div className="thuloDiv">
+        hello
         <h6>Green Apple</h6>
         <div className="price">
           <h5>$14.99</h5>
